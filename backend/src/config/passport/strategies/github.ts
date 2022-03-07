9488270a59a457a31d3466e574ca519055fb85eb
Wil Gerard <williamrgerard@gmail.com>
2022-03-07 17:12:26 -0600

    chore: add github oauth production environment variables

--- backend/src/config/passport/strategies/github.ts
+++ backend/src/config/passport/strategies/github.ts
@@ -1,15 +1,22 @@
 const GitHubStrategy = require('passport-github2').Strategy;
 
+let gitHubClientID = ''
+let gitHubClientSecret = ''
+
+if (process.env.NODE_ENV === 'production') {
+  gitHubClientID = `${process.env.PRODUCTION_GITHUB_CLIENT_ID}`
+  gitHubClientSecret = `${process.env.PRODUCTION_GITHUB_CLIENT_SECRET}`
+} else {
+  gitHubClientID = `${process.env.GITHUB_CLIENT_ID}`
+  gitHubClientSecret = `${process.env.GITHUB_CLIENT_SECRET}`
+  }
+
 const gitHubStrategySettings: any = {
-  clientID: `${process.env.GITHUB_CLIENT_ID}`,
-  clientSecret: `${process.env.GITHUB_CLIENT_SECRET}`,
+  clientID: gitHubClientID,
+  clientSecret: gitHubClientSecret,
   callbackURL: '/api/auth/github/callback',
   passReqToCallback: true,
-  scope: [
-    'user:follow',
-    'user:email',
-    'read:user'
-  ]
+  scope: ['user:follow', 'user:email', 'read:user'],
 };
 export const gitHubStrategy: any = new GitHubStrategy(
   gitHubStrategySettings,
